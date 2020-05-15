01333c061f2a95de7686f8ff1bdc46949df9c8c3
smallbee <smallbee3@gmail.com>
2020-05-16 01:17:14 +0900

    Drop all existing tables by sending raw SQL query in afterAll

--- tests/testSetup.ts
+++ tests/testSetup.ts
@@ -1,8 +1,10 @@
 import { Http2Server } from 'http2';
+import { PrismaClient } from '@prisma/client';
 import { createApp } from '../src/app';
 import express from 'express';
 import { startServer } from '../src/server';
 
+const prisma = new PrismaClient();
 const port = 4000;
 let server: Http2Server;
 export const testHost = `http://localhost:${port}/graphql`;
@@ -14,5 +16,6 @@ beforeAll(async (done) => {
 });
 
 afterAll(async () => {
+  await prisma.raw('DROP schema test CASCADE');
   server.close();
 });
