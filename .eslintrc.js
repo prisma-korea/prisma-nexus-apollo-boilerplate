a936bbf2aff24ee2b66377a4bf97f4957c481392
hyochan <dooboolab@gmail.com>
2020-06-28 13:58:09 +0900

    Small eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
     'comma-dangle': ['error', 'always-multiline'],
     semi: [2, 'always'],
     'arrow-parens': ['error', 'always'],
-    'space-before-function-paren': ['error', 'never'],
     'no-new-object': 'error',
     'no-array-constructor': 'error',
     'sort-imports': [
