fe30b6490d60ec5dac5d22d1314a54b90dbe19f3
hyochan <dooboolab@gmail.com>
2021-03-14 14:28:38 +0900

    Updated packages - Prisma 2.18.0

--- prisma/migrations/20210314052709_init/migration.sql
+++ prisma/migrations/20210314052709_init/migration.sql
@@ -1,21 +1,21 @@
 -- CreateEnum
-CREATE TYPE "sample"."Gender" AS ENUM ('male', 'female');
+CREATE TYPE "Gender" AS ENUM ('male', 'female');
 
 -- CreateEnum
-CREATE TYPE "sample"."AuthType" AS ENUM ('email', 'facebook', 'google', 'apple');
+CREATE TYPE "AuthType" AS ENUM ('email', 'facebook', 'google', 'apple');
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT,
-    "name" TEXT,
-    "nickname" TEXT,
-    "thumbURL" TEXT,
-    "photoURL" TEXT,
+    "id" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255),
+    "name" VARCHAR(255),
+    "nickname" VARCHAR(255),
+    "thumbURL" VARCHAR(255),
+    "photoURL" VARCHAR(255),
     "birthDay" TIMESTAMP(3),
     "gender" "Gender",
-    "phone" TEXT,
+    "phone" VARCHAR(255),
     "verified" BOOLEAN,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
@@ -26,21 +26,21 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Profile" (
-    "id" TEXT NOT NULL,
+    "id" VARCHAR(255) NOT NULL,
     "socialId" TEXT,
     "authType" "AuthType",
-    "userId" TEXT NOT NULL,
+    "userId" VARCHAR(255) NOT NULL,
 
     PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Post" (
-"id" SERIAL,
-    "title" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
-    "userId" TEXT NOT NULL,
+    "userId" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "deletedAt" TIMESTAMP(3),
@@ -55,7 +55,7 @@ CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
 CREATE UNIQUE INDEX "Profile.userId_unique" ON "Profile"("userId");
 
 -- AddForeignKey
-ALTER TABLE "Profile" ADD FOREIGN KEY("userId")REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Profile" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Post" ADD FOREIGN KEY("userId")REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Post" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
