1d8effc20478cc6cee7f677480fc2aa2cbfad6dc
Hyo <dooboolab@gmail.com>
2021-06-28 13:27:19 +0900

    Update packages

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import {createContext, prisma} from './context';
+import {Server, createServer as createHttpServer} from 'http';
 
 import {ApolloServer} from 'apollo-server-express';
-import {Http2Server} from 'http2';
-import {PrismaClient} from '@prisma/client';
 import {applyMiddleware} from 'graphql-middleware';
 import {createApp} from './app';
-import {createServer as createHttpServer} from 'http';
+import {createContext} from './context';
 import express from 'express';
 import {permissions} from './permissions';
 import {schema} from './schema';
@@ -14,15 +12,18 @@ const {PORT = 5000, NODE_ENV} = process.env;
 
 const schemaWithMiddleware = applyMiddleware(schema, permissions);
 
-const createApolloServer = (prisma: PrismaClient): ApolloServer =>
+const createApolloServer = (): ApolloServer =>
   new ApolloServer({
     schema: schemaWithMiddleware,
-    context: (req) => createContext(prisma, req),
-    introspection: NODE_ENV !== 'production',
+    context: createContext,
+    introspection: process.env.NODE_ENV !== 'production',
     playground: process.env.NODE_ENV !== 'production',
     subscriptions: {
-      onConnect: (): void => {
+      onConnect: async (connectionParams, _webSocket, _context) => {
         process.stdout.write('Connected to websocket\n');
+
+        // Return connection parameters for context building.
+        return {connectionParams};
       },
     },
   });
@@ -30,46 +31,34 @@ const createApolloServer = (prisma: PrismaClient): ApolloServer =>
 const initializeApolloServer = (
   apollo: ApolloServer,
   app: express.Application,
-  port: string | number,
 ): (() => void) => {
   apollo.applyMiddleware({app});
 
   return (): void => {
-    if (process.env.NODE_ENV !== 'test')
-      process.stdout.write(
-        `🚀 Server ready at http://localhost:${port}${apollo.graphqlPath}\n`,
-      );
+    process.stdout.write(
+      `🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}\n`,
+    );
   };
 };
 
 export const startServer = async (
   app: express.Application,
-  port: string | number,
-): Promise<Http2Server> => {
+  port?: number,
+): Promise<Server> => {
   const httpServer = createHttpServer(app);
-  const apollo = createApolloServer(prisma);
-
-  const handleApolloServerInitilized = initializeApolloServer(
-    apollo,
-    app,
-    port,
-  );
+  const apollo = createApolloServer();
 
   apollo.installSubscriptionHandlers(httpServer);
 
-  // Disconnect prisma client on server close.
-  httpServer.addListener('close', () => prisma.$disconnect());
+  const handleApolloServerInitilized = initializeApolloServer(apollo, app);
 
-  return new Promise((resolve) => {
-    httpServer.listen({port}, () => {
-      handleApolloServerInitilized();
-      resolve(httpServer);
-    });
+  return httpServer.listen({port}, () => {
+    handleApolloServerInitilized();
   });
 };
 
 if (NODE_ENV !== 'test') {
   const app = createApp();
 
-  startServer(app, PORT);
+  startServer(app);
 }
