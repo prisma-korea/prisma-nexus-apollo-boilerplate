d51b2ef95c4d6758fca4c1e94f07354e78afd1a3
hyochan <dooboolab@gmail.com>
2022-06-03 20:43:36 +0900

    Migrate tests

--- tests/testSetup.ts
+++ tests/testSetup.ts
@@ -1,22 +1,27 @@
+import 'cross-fetch/polyfill';
+
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client/core';
 import {TestUtils, getTestUtils, setTestUtils} from './testUtils';
 
-import ApolloClient from 'apollo-client';
 import {GraphQLClient} from 'graphql-request';
-import {Headers} from 'cross-fetch';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import {GraphQLWsLink} from '@apollo/client/link/subscriptions';
 import NodeWebSocket from 'ws';
 import {PrismaClient} from '@prisma/client';
-import {SubscriptionClient} from 'subscriptions-transport-ws';
-import {WebSocketLink} from 'apollo-link-ws';
 import {assert} from '../src/utils/assert';
 import {createApp} from '../src/app';
+import {createClient} from 'graphql-ws';
 import {execSync} from 'child_process';
 import express from 'express';
-import path from 'path';
+import {getMainDefinition} from '@apollo/client/utilities';
 import {startServer} from '../src/server';
 
 // @ts-ignore
-global.Headers = global.Headers || Headers;
+// global.Headers = global.Headers || Headers;
 
 const {PORT = 5566, DATABASE_URL} = process.env;
 
@@ -39,14 +44,32 @@ beforeAll(async () => {
   // Instantiate graphql client.
   const graphqlClient = new GraphQLClient(testHost);
 
-  const networkInterface = new SubscriptionClient(
-    testSubscriptionHost,
-    {reconnect: true},
-    NodeWebSocket,
+  const httpLink = new HttpLink({
+    uri: testHost,
+  });
+
+  const networkInterface = createClient({
+    url: testSubscriptionHost,
+    webSocketImpl: NodeWebSocket,
+  });
+
+  const wsLink = new GraphQLWsLink(networkInterface);
+
+  const splitLink = split(
+    ({query}) => {
+      const definition = getMainDefinition(query);
+
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
   );
 
   const apolloClient = new ApolloClient({
-    link: new WebSocketLink(networkInterface),
+    link: splitLink,
     cache: new InMemoryCache(),
   });
 
@@ -74,7 +97,7 @@ afterAll(async () => {
       }
     });
 
-    networkInterface.close();
+    networkInterface.dispose();
   });
 
   // Disconnect prisma client.
