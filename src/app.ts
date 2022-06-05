21d52d8fcb3a9850fae71b540645bc98e3b94c5a
hyochan <dooboolab@gmail.com>
2022-06-06 00:49:47 +0900

    Install altair

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, {Express} from 'express';
 
+import {altairExpress} from 'altair-express-middleware';
 import cors from 'cors';
 import ejs from 'ejs';
 import path from 'path';
+
+const {PORT = 6001} = process.env;
 
 export const createApp = (): Express => {
   const app = express();
@@ -12,6 +15,14 @@ export const createApp = (): Express => {
   app.get('/', (req, res) => {
     res.send('It works - ver. 0.0.1');
   });
+
+  app.use(
+    '/altair',
+    altairExpress({
+      endpointURL: '/graphql',
+      subscriptionsEndpoint: `ws://localhost:${PORT}/graphql`,
+    }),
+  );
 
   app.set('views', path.join(__dirname, '../html'));
   app.engine('html', ejs.renderFile);
